e0dafd59e0d61833323ecd4d5d5ac4fde86618d8
Ayanauwu <ayana.temirbekova@gmail.com>
2021-12-17 11:34:20 +0600

    qwerty

--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -1,3 +1,3 @@
-export const PRODUCTS_API = "http://localhost:8000/products";
+export const PRODUCTS_API = "https://online-mag-yn.herokuapp.com/apiproducts";
 
-export const BRANDS_API = "http://localhost:8000/brands";
+export const BRANDS_API = "https://online-mag-yn.herokuapp.com/apibrands";
